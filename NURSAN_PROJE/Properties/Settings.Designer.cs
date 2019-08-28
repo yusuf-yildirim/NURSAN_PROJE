c3e2b24719f0c653eeb4fe721ecda2fa580a60fa
BurakcanKolay <kolay_burakcan@hotmail.com>
2019-08-28 23:29:27 +0300

    Db

--- NURSAN_PROJE/Properties/Settings.Designer.cs
+++ NURSAN_PROJE/Properties/Settings.Designer.cs
@@ -22,15 +22,5 @@ namespace NURSAN_PROJE.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost;Initial Catalog=master;Integrated Security=True")]
-        public string SqLite {
-            get {
-                return ((string)(this["SqLite"]));
-            }
-        }
     }
 }
