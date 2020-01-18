65609dc2cd84ca415f43c0c67b4c641b454f4cb0
BurakcanKolay <kolay_burakcan@hotmail.com>
2020-01-18 20:57:23 +0300

    Components Update Methods

--- NURSAN_PROJE/SQL/Components.cs
+++ NURSAN_PROJE/SQL/Components.cs
@@ -48,11 +48,71 @@ namespace NURSAN_PROJE.SQL
         ///</summary>
         public void addComponent(String technicname, String name, int testCurrent, int testCurrentMultiplier, int voltageDrop, int tolerence)
         {
-
-
             getFromLocalTablesmain("Components").Rows.Add(Guid.NewGuid().ToString(), name, technicname, testCurrent, testCurrentMultiplier, tolerence, null, null, voltageDrop, 0, 0, 0, 0);
+        }
+
+
+        //  UPDATE
+        ///<summary>
+        ///Splice güncellemesi için kullanılır.
+        ///</summary>
+        public void updateComponent(string componentID , String name)
+        {
+            var row = getFromLocalTablesmain("Components").Select("ID_component = '" + componentID + "'")[0];
+            row[1] = name;
+
+        }
+        ///<summary>
+        ///Kapasitör ve direnç güncellemesi için kullanılır.
+        ///</summary>
+        public void updateComponent(string componentID, String name, int value, int valuemultiplier, int tolerence)
+        {
+            var row = getFromLocalTablesmain("Components").Select("ID_component = '" + componentID + "'")[0];
+            row[1] = name;
+            row[3]= value;
+            row[4] = valuemultiplier;
+            row[5] = tolerence;            
+        }
+        ///<summary>
+        ///Diyot güncellemesi için kullanılır.
+        ///</summary>
+        public void updateComponent(string componentID,  String name, int forwardvoltage, int tolerence)
+        {
+            var row = getFromLocalTablesmain("Components").Select("ID_component = '" + componentID + "'")[0];
+            row[1] = name;
+            row[3] = forwardvoltage;
+            row[5] = tolerence;
            
         }
+        ///<summary>
+        ///Termistör güncellemesi için kullanılır.
+        ///</summary>
+        public void updateComponent(string componentID,  String name, int Comparasiontolerence, int firsttestpoint, int secondtestpoint, int minResistance, int maxResistence, int minResistincemultiplier, int maxResistenceMultiplier)
+        {
+            var row = getFromLocalTablesmain("Components").Select("ID_component = '" + componentID + "'")[0];
+            row[1] = name;
+            row[6] = firsttestpoint.ToString();
+            row[7] = secondtestpoint.ToString();
+            row[8] = minResistance;
+            row[9] = minResistincemultiplier;
+            row[10] = maxResistence;
+            row[11] = Comparasiontolerence;
+            row[12] = maxResistenceMultiplier;
+        }
+        ///<summary>
+        ///Generic Componenet güncellemesi için kullanılır.
+        ///</summary>
+        public void updateComponent(string componentID,  String name, int testCurrent, int testCurrentMultiplier, int voltageDrop, int tolerence)
+        {
+            var row = getFromLocalTablesmain("Components").Select("ID_component = '" + componentID + "'")[0];
+            row[1] = name;
+            row[3] = testCurrent;
+            row[4] = testCurrentMultiplier;
+            row[5] = tolerence;
+            row[8] = voltageDrop;
+        }
+
+
         public void componenet2project(string ComponentID)
         {
             if(getFromLocalTablesproject("PComponents").Select("ID_component='" + ComponentID + "'").Length > 0)
