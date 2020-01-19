8a990cc3ffc828239ffef1f5da3f30757337a564
Burakcan Kolay <kolayburakcan@gmail.com>
2020-01-19 18:41:19 +0300

    Phase delete method #13

--- NURSAN_PROJE/SQL/Phase.cs
+++ NURSAN_PROJE/SQL/Phase.cs
@@ -16,7 +16,17 @@ namespace NURSAN_PROJE.SQL
             getFromLocalTablesproject("tbl_etap").Rows.Add(Guid.NewGuid().ToString(), phaseName);
         }
 
+        public void deletePhase(string PhaseID)
+        {
+           var rows =  getFromLocalTablesproject("PConnections").Select("ID_etap_1 = '"+PhaseID+"'");
 
+            foreach(var row in rows)
+            {
+                row.Delete();
+            }
+            getFromLocalTablesproject("tbl_etap").Select("ID_etap = '" + PhaseID + "'")[0].Delete();
+
+        }
 
 
     }
