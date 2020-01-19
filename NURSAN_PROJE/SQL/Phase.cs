0aea3c29888b74a21cfd083d9026d91ed9767216
Burakcan Kolay <kolayburakcan@gmail.com>
2020-01-19 18:45:45 +0300

    user confirmation panel for Phase delete

--- NURSAN_PROJE/SQL/Phase.cs
+++ NURSAN_PROJE/SQL/Phase.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using DevExpress.Utils.CommonDialogs.Internal;
+using DevExpress.XtraEditors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace NURSAN_PROJE.SQL
 {
@@ -18,14 +21,22 @@ namespace NURSAN_PROJE.SQL
 
         public void deletePhase(string PhaseID)
         {
-           var rows =  getFromLocalTablesproject("PConnections").Select("ID_etap_1 = '"+PhaseID+"'");
-
-            foreach(var row in rows)
+            System.Windows.Forms.DialogResult rs = XtraMessageBox.Show("Fazı ve içindeki bağlantıları Kalıcı olarak silmek üzeresiniz EMİN MİSİNİZ ?", "Silme Bilgisi", MessageBoxButtons.YesNo);
+            if (rs == System.Windows.Forms.DialogResult.Yes)
             {
-                row.Delete();
-            }
-            getFromLocalTablesproject("tbl_etap").Select("ID_etap = '" + PhaseID + "'")[0].Delete();
+                var rows = getFromLocalTablesproject("PConnections").Select("ID_etap_1 = '" + PhaseID + "'");
 
+                foreach (var row in rows)
+                {
+                    row.Delete();
+                }
+                getFromLocalTablesproject("tbl_etap").Select("ID_etap = '" + PhaseID + "'")[0].Delete();
+
+            }
+            else
+            {
+                XtraMessageBox.Show("İşlem iptal edildi!", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
 
